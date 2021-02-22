b5d122d84367eee0f126b5114c0c773d5f6dcda7
Dmitriy Rybnikov <senior.ribnickow1985@yandex.ru>
2021-02-22 17:26:43 +0700

    add type script

--- next.config.js
+++ next.config.js
@@ -1,15 +1,3 @@
 const withImages = require('next-images');
-const path = require('path');
 
-module.exports = withImages({
-    webpack: config => {
-    config.resolve.modules.push(path.resolve('/'))
-
-    return config
-  },
-  // sassOptions: {
-  //   includePaths: [path.join('styles')],
-  //   prependData: `@import "mixins.scss"; @import "variables.scss";`,
-  // }
-})
-
+module.exports = withImages({})
